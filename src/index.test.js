863a17d128e97d9d9cb0fbb151b840bfb7477ac8
Saikat <saikats_5@yahoo.co.in>
2017-08-02 09:59:13 +0530

    second test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,20 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe("First Test",()=>{
     it("Should Pass",()=>{
         expect(true).to.equal(true);
+    });
+});
+
+describe("index.html",()=>{
+    it("Starting text",()=>{
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        jsdom.env(index, function(err, window){
+            const div = window.document.getElementsByTagName('div')[0];
+            expect(div.innerHTML).to.equal("Testing Public URL");
+            window.close();
+        });
     });
 });
